38ebfe0b2a3e45a4af13425a8b5bb3d883db5810
Robert M <belamorris@gmail.com>
2019-02-13 17:31:30 -0700

    letter js i think is good time to start word js

---
+++ Letter.js
@@ -0,0 +1,22 @@
+var Letter = function(newLetter) {
+	this.answer = newLetter.toLowerCase();
+	this.solved = false;
+
+	//always display spaces
+	if (this.answer == " ")
+	 	this.solved = true;
+
+	//function that returns letter if solved or _ otherwise
+	this.returnLetter = function() {
+		if (this.solved)
+			return this.answer;
+		else
+			return "_";
+	}
+
+	//function that changes solved if guess is correct
+	this.guess = function(input) {
+		if (input.toLowerCase() === this.answer)
+			this.solved = true;
+	};
+}
